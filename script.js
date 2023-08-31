eba995940b63246cd887626ec27b33c441810aee
marwa maky <129126532+ahmedmansour999@users.noreply.github.com>
2023-08-31 03:35:26 +0300

    data

---
+++ script.js
@@ -0,0 +1,55 @@
+// nav bar 
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) { // You can adjust the scroll value as needed
+            $(".navbar").addClass("scrolled");
+        } else {
+            $(".navbar").removeClass("scrolled");
+        }
+    });
+});
+
+
+// container 5
+
+// card paragraph view 
+const buttonArticle = document.getElementById('buttonArticle') ; 
+const paragraph = document.getElementById('paragraph')
+const eye = document.getElementById('eye') ;
+const buttonArticle1 = document.getElementById('buttonArticle1') ; 
+const paragraph1 = document.getElementById('paragraph1') ;
+const bullseye = document.getElementById('bullseye') ;
+const buttonArticle2= document.getElementById('buttonArticle2') ; 
+const paragraph2 = document.getElementById('paragraph2') ;
+const chart = document.getElementById('chart') ;
+
+
+
+buttonArticle.onclick = ()=>{
+    if (paragraph.style.display === "none") {
+        paragraph.style.display = 'block'
+        eye.style.color= "red"
+    }else{
+        paragraph.style.display = "none"
+        eye.style.color= "rgb(2 11 56)"
+    }
+}
+buttonArticle1.onclick = ()=>{
+    if (paragraph1.style.display === "none") {
+        paragraph1.style.display = 'block'
+        bullseye.style.color= "red"
+    }else{
+        paragraph1.style.display = "none"
+        bullseye.style.color= "rgb(2 11 56)"
+    }
+}
+buttonArticle2.onclick = ()=>{
+    if (paragraph2.style.display === "none") {
+        paragraph2.style.display = 'block'
+        chart.style.color= "red"
+    }else{
+        paragraph2.style.display = "none"
+        chart.style.color= "rgb(2 11 56)"
+    }
+}
