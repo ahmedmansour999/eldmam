65c2c2f427b71da7ff7a718c2db53865e70f07b8
marwa maky <129126532+ahmedmansour999@users.noreply.github.com>
2023-09-01 15:10:34 +0300

    Addition

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// Get the modal element
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Display the modal when the page loads
+window.onload = function () {
+    modal.style.display = "block";
+}
+
+// Close the modal when the user clicks on the <span> element
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// Close the modal when the user clicks anywhere outside of it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
+
+
+
+
 // nav bar 
 
 $(document).ready(function() {
